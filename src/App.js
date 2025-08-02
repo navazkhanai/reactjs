1ca1990afc02f0262c290216c2edb02c254bebe7
navazkhanai <navazkhanai@gmail.com>
2025-08-02 18:45:57 +0530

    Add App.js component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to your ReactJS Project!</h1>
+      <p>This is the main App component.</p>
+    </div>
+  );
+}
+
+export default App;
